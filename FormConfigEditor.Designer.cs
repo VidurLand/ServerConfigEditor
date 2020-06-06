5dc4c615ad8541265ced0731edb7a346be60a47c
Vidur <vidur@mail.ru>
2020-06-06 19:38:27 +0300

    Добавьте файлы проекта.

---
+++ FormConfigEditor.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace ServerConfigEditor
+{
+    partial class FormConfigEditor
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBoxFile = new System.Windows.Forms.GroupBox();
+            this.groupBoxPath = new System.Windows.Forms.GroupBox();
+            this.buttonReadFile = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.buttonList = new System.Windows.Forms.Button();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonEdit = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.groupBoxFile.SuspendLayout();
+            this.groupBoxPath.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBoxFile
+            // 
+            this.groupBoxFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBoxFile.Controls.Add(this.button2);
+            this.groupBoxFile.Controls.Add(this.buttonReadFile);
+            this.groupBoxFile.Location = new System.Drawing.Point(899, 89);
+            this.groupBoxFile.Name = "groupBoxFile";
+            this.groupBoxFile.Size = new System.Drawing.Size(200, 335);
+            this.groupBoxFile.TabIndex = 0;
+            this.groupBoxFile.TabStop = false;
+            this.groupBoxFile.Text = "Файл";
+            // 
+            // groupBoxPath
+            // 
+            this.groupBoxPath.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBoxPath.Controls.Add(this.buttonDelete);
+            this.groupBoxPath.Controls.Add(this.buttonEdit);
+            this.groupBoxPath.Controls.Add(this.buttonAdd);
+            this.groupBoxPath.Controls.Add(this.buttonList);
+            this.groupBoxPath.Location = new System.Drawing.Point(899, 430);
+            this.groupBoxPath.Name = "groupBoxPath";
+            this.groupBoxPath.Size = new System.Drawing.Size(200, 392);
+            this.groupBoxPath.TabIndex = 1;
+            this.groupBoxPath.TabStop = false;
+            this.groupBoxPath.Text = "Путь";
+            // 
+            // buttonReadFile
+            // 
+            this.buttonReadFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonReadFile.Location = new System.Drawing.Point(40, 34);
+            this.buttonReadFile.Name = "buttonReadFile";
+            this.buttonReadFile.Size = new System.Drawing.Size(75, 23);
+            this.buttonReadFile.TabIndex = 0;
+            this.buttonReadFile.Text = "Прочитать";
+            this.buttonReadFile.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(40, 141);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // buttonList
+            // 
+            this.buttonList.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonList.Location = new System.Drawing.Point(25, 66);
+            this.buttonList.Name = "buttonList";
+            this.buttonList.Size = new System.Drawing.Size(75, 23);
+            this.buttonList.TabIndex = 0;
+            this.buttonList.Text = "Список";
+            this.buttonList.UseVisualStyleBackColor = true;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonAdd.Location = new System.Drawing.Point(25, 146);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 1;
+            this.buttonAdd.Text = "Добавить";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            // 
+            // buttonEdit
+            // 
+            this.buttonEdit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonEdit.Location = new System.Drawing.Point(25, 236);
+            this.buttonEdit.Name = "buttonEdit";
+            this.buttonEdit.Size = new System.Drawing.Size(75, 23);
+            this.buttonEdit.TabIndex = 2;
+            this.buttonEdit.Text = "Изменить";
+            this.buttonEdit.UseVisualStyleBackColor = true;
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonDelete.Location = new System.Drawing.Point(25, 311);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(75, 23);
+            this.buttonDelete.TabIndex = 3;
+            this.buttonDelete.Text = "Удалить";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            // 
+            // FormConfigEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1111, 834);
+            this.Controls.Add(this.groupBoxFile);
+            this.Controls.Add(this.groupBoxPath);
+            this.Name = "FormConfigEditor";
+            this.Text = "ConfigEditor";
+            this.groupBoxFile.ResumeLayout(false);
+            this.groupBoxPath.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxFile;
+        private System.Windows.Forms.GroupBox groupBoxPath;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button buttonReadFile;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonEdit;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonList;
+    }
+}
+
